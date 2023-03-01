1507060b98f5e68b545a9b38cabb092f9ad0e9c2
secanno <secanno@gmail.com>
2023-03-01 11:20:33 -0800

    Correcting estimate labels

--- Figure3_CoefficientEstimates.R
+++ Figure3_CoefficientEstimates.R
@@ -4,12 +4,14 @@ library(ggplot2)
 library(ggthemes)
 library(stringr)
 
-ggplot(data, aes(x = Variable, y = Genus, fill = Estimate)) + 
+
+ggplot(data_long, aes(x = Var, y = Genus, fill = Estimate)) + 
   geom_tile(color = 'black', size = 0.2) +
-  geom_text(aes(fill = Estimate, label = round(Estimate, 2)), color = "black", size = 3) + #add numbers to the boxes
+  geom_text(data = data_long, aes(label = sprintf("%0.1f", round(Estimate, digits = 1))), 
+            size = 2.5, color = "grey28") + #add numbers to the boxes
   xlim(levels(data_long$Var)) + 
   facet_grid(Division ~., switch = 'y', scales = "free_y", space = "free_y", drop = TRUE) +
-  scale_fill_gradient2(low = "#0033FF", mid = "#FFFFFF", high = "#CC3300") + 
+  scale_fill_gradient2(low = "#CC3300", mid = "white", high = "#0033FF") + 
   theme_few() +
   theme(axis.title.y = element_blank(),
         axis.title.x = element_blank(),
@@ -18,3 +20,4 @@ ggplot(data, aes(x = Variable, y = Genus, fill = Estimate)) +
         axis.ticks = element_blank(),
         legend.position = "right") +
   scale_x_discrete(labels = function(x) str_wrap(x, width = 15))
+
